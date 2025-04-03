72166e853a33aadaa5a168752c13b34adc89cc0f
Alice (KlimovaNK) <natusyaklimova25@gmail.com>
2025-04-04 02:44:48 +0300

    change: added maximum of digits

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n'<< "A - B = " << a - b << '\n';
+    cout << "A + B = " << a + b << '\n'
+     << "A - B = " << a - b << '\n'
+     << "A * B = " << a * b << '\n'
+     << "A / B = " << a / b << '\n';
+    if (a < b) cout << "Maximim is A: " << a << endl;
+    else cout << "Maximim is B: " << b << endl;
     return 0;
 }
